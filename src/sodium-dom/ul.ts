0338fb230e980456d1911af9d006209548ffd8e0
cubuspl42 <cubuspl42@gmail.com>
2020-08-03 21:09:00 +0200

    Implement todo adding

--- src/sodium-dom/ul.ts
+++ src/sodium-dom/ul.ts
@@ -1,17 +1,45 @@
 import { NaElement, NaElementProps, NaNode } from "./dom";
-import { buildElementWithChildren } from "./utils";
-import { NaGenericElement } from "./genericElement";
+import { CellOr, cellOrToCell, linkChildrenC, linkClassName } from "./utils";
+import { Cell } from "sodiumjs";
+import { LazyGetter } from "lazy-get-decorator";
 
-export function ul(props: NaElementProps, ...children: ReadonlyArray<NaNode>): NaElement;
-export function ul(...children: ReadonlyArray<NaNode>): NaElement;
+export class NaUlElement extends NaElement {
+	private readonly props?: NaElementProps;
+
+	private readonly children: Cell<ReadonlyArray<NaNode>>;
+
+	constructor(
+		props: NaElementProps | undefined,
+		children: Cell<ReadonlyArray<NaNode>>,
+	) {
+		super();
+		this.props = props;
+		this.children = children;
+	}
+
+	@LazyGetter()
+	get htmlElement(): HTMLElement {
+		const element = document.createElement("ul");
+		linkClassName(element, this.props);
+		linkChildrenC(element, this.children);
+		return element;
+	}
+}
+
+
+export function ul(props: NaElementProps, children: ReadonlyArray<NaNode>): NaElement;
+export function ul(children: ReadonlyArray<NaNode>): NaElement;
+
+export function ul(props: NaElementProps, children: Cell<ReadonlyArray<NaNode>>): NaElement;
+export function ul(children: Cell<ReadonlyArray<NaNode>>): NaElement;
 
 export function ul(
-	arg0: NaElementProps | NaNode,
-	...children: ReadonlyArray<NaNode>
+	arg0: NaElementProps | CellOr<ReadonlyArray<NaNode>>,
+	arg1?: CellOr<ReadonlyArray<NaNode>>,
 ): NaElement {
-	return buildElementWithChildren(
-		arg0,
-		children,
-		(p, c) => new NaGenericElement("ul", p, c),
-	);
+	if (arg0 instanceof Cell || arg0 instanceof Array) {
+		return new NaUlElement(undefined, cellOrToCell(arg0));
+	} else {
+		return new NaUlElement(arg0 as NaElementProps, cellOrToCell(arg1!));
+	}
 }
--- src/sodium-dom/utils.ts
+++ src/sodium-dom/utils.ts
@@ -3,12 +3,19 @@ import { Cell } from "sodiumjs";
 
 export type CellOr<A> = Cell<A> | A;
 
+export function cellOrToCell<A>(cellOr: CellOr<A>): Cell<A> {
+	if (cellOr instanceof Cell) {
+		return cellOr;
+	} else {
+		return new Cell(cellOr);
+	}
+}
+
 export function buildNode(element: NaNode): Node {
 	return element instanceof NaElement ?
 		element.htmlElement :
 		document.createTextNode(element);
 }
-
 
 export function buildElementWithChildren<TElementProps extends NaElementProps, TElement extends NaElement>(
 	arg0: TElementProps | NaNode,
@@ -22,9 +29,22 @@ export function buildElementWithChildren<TElementProps extends NaElementProps, T
 	}
 }
 
+function clearChildren(element: HTMLElement) {
+	while (element.firstChild) {
+		element.removeChild(element.lastChild!);
+	}
+}
 
 export function linkChildren(htmlElement: HTMLElement, children: ReadonlyArray<NaNode>) {
 	children.forEach((child) => htmlElement.appendChild(buildNode(child)));
+}
+
+export function linkChildrenC(htmlElement: HTMLElement, children: Cell<ReadonlyArray<NaNode>>) {
+	// TODO: Unlisten
+	children?.listen((c) => {
+		clearChildren(htmlElement);
+		c.forEach((child) => htmlElement.appendChild(buildNode(child)));
+	});
 }
 
 export function linkClassName(htmlElement: HTMLElement, props: NaElementProps | undefined) {
