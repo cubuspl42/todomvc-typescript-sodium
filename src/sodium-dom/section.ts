f76d37c3bcc02c1dd5c14abcf852ffc029a810c1
cubuspl42 <cubuspl42@gmail.com>
2020-08-08 21:46:17 +0200

    Add another overload for `section`

--- src/sodium-dom/section.ts
+++ src/sodium-dom/section.ts
@@ -1,5 +1,5 @@
 import { NaElement, NaElementProps, NaNode } from "./dom";
-import { buildGenericElementWithChildrenC, CellOr } from "./utils";
+import { buildGenericElementWithChildrenC, CellOr, NaElementChildren } from "./utils";
 import { Cell } from "sodiumjs";
 
 export function section(props: NaElementProps, children: ReadonlyArray<NaNode>): NaElement;
@@ -8,9 +8,12 @@ export function section(children: ReadonlyArray<NaNode>): NaElement;
 export function section(props: NaElementProps, children: Cell<ReadonlyArray<NaNode>>): NaElement;
 export function section(children: Cell<ReadonlyArray<NaNode>>): NaElement;
 
+export function section(props: NaElementProps, children: ReadonlyArray<NaNode | Cell<NaNode | null>>): NaElement;
+export function section(children: ReadonlyArray<NaNode | Cell<NaNode | null>>): NaElement;
+
 export function section(
-	arg0: NaElementProps | CellOr<ReadonlyArray<NaNode>>,
-	arg1?: CellOr<ReadonlyArray<NaNode>>,
+	arg0: NaElementProps | NaElementChildren,
+	arg1?: NaElementChildren,
 ): NaElement {
 	return buildGenericElementWithChildrenC(arg0, arg1, "section");
 }
